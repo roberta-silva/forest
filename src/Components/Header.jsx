1b7cab44acf1f336f43f486c71bd42de3eb3d38e
Roberta Silva <robertadasilva.contato@gmail.com>
2026-05-29 15:06:35 -0300

    menu mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,39 @@
+import React from 'react';
 import Forest from '../assets/img/forest.svg?react';
 
 const Header = () => {
+  const [menuMobile, setMenuMobile] = React.useState(false);
+
+  function handleClick() {
+    setMenuMobile((prev) => !prev);
+  }
+  function closeMenu() {
+    setMenuMobile(false);
+  }
+
   return (
     <header className="container flex items-center justify-between gap-8 py-8">
       <a href="/">
         <Forest />
       </a>
 
-      <nav>
-        <ul className="text-white text-xl flex flex-wrap gap-8">
+      <nav
+        className={`
+    lg:block items-center z-40
+    max-lg:fixed max-lg:w-full max-lg:inset-0
+    ${menuMobile ? 'block' : 'hidden'}
+  `}
+      >
+        <div
+          onClick={closeMenu}
+          className={`inset-0 bg-verde-950/40 backdrop:blur-md ${menuMobile ? 'fixed' : 'hidden'}`}
+        ></div>
+
+        <ul className="text-white text-2xl max-lg:divide-y-2 max-lg:divide-white/10 max-lg:z-50 max-lg:absolute max-lg:p-8 max-lg:w-full lg:flex lg:flex-wrap lg:gap-8 lg:text-xl">
           <li>
             <a
-              className="hover:underline hover:underline-offset-8"
+              onClick={closeMenu}
+              className="p-4 lg:px-0 lg:py-2 block max-lg:hover:bg-white/10 lg:hover:underline lg:hover:underline-offset-8"
               href="#acomodacoes"
             >
               Acomodações
@@ -19,7 +41,8 @@ const Header = () => {
           </li>
           <li>
             <a
-              className="hover:underline hover:underline-offset-8"
+              onClick={closeMenu}
+              className="p-4 lg:px-0 lg:py-2 block max-lg:hover:bg-white/10 lg:hover:underline lg:hover:underline-offset-8"
               href="#eventos"
             >
               Eventos
@@ -27,7 +50,8 @@ const Header = () => {
           </li>
           <li>
             <a
-              className="hover:underline hover:underline-offset-8"
+              onClick={closeMenu}
+              className="p-4 lg:px-0 lg:py-2 block max-lg:hover:bg-white/10 lg:hover:underline lg:hover:underline-offset-8"
               href="#experiencias"
             >
               Experiências
@@ -35,7 +59,8 @@ const Header = () => {
           </li>
           <li>
             <a
-              className="hover:underline hover:underline-offset-8"
+              onClick={closeMenu}
+              className="p-4 lg:px-0 lg:py-2 block max-lg:hover:bg-white/10 lg:hover:underline lg:hover:underline-offset-8"
               href="#contato"
             >
               Contato
@@ -43,6 +68,21 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+
+      <button
+        onClick={handleClick}
+        className="flex items-center gap-3 bg-verde-400 text-verde-900 py-2 px-4 rounded-full lg:hidden cursor-pointer"
+      >
+        Menu
+        <span
+          className="h-3 w-4 flex flex-col justify-between
+        *:h-0.5 *:rounded-md *:bg-verde-800"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </button>
     </header>
   );
 };
